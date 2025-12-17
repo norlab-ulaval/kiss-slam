37931eec1e6332a6a115da1f68686406fbe1e18c
Nicolas Lauzon <nicolas_lauzon@outlook.com>
2025-12-17 13:31:01 -0500

    Simple ros2 node that matches the cli pipeline

--- kiss_slam/ros/src/kiss_slam_ros/kiss_slam_ros/kiss_slam_node.py
+++ kiss_slam/ros/src/kiss_slam_ros/kiss_slam_ros/kiss_slam_node.py
@@ -24,26 +24,26 @@ import os
 
 import numpy as np
 import rclpy
-from ament_index_python.packages import get_package_share_directory
 from kiss_slam_ros.conversions import (
     build_map,
     build_odometry,
     build_transform,
     matrix_to_pose,
     pc2_to_numpy,
-    slam_config_from_params,
-    slam_params_from_config,
 )
-from map_closures import map_closures
-from nav_msgs.msg import OccupancyGrid, Odometry
-from rcl_interfaces.msg import ParameterDescriptor, SetParametersResult
+
+from pyquaternion import Quaternion
+
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs_py import point_cloud2
+
+from kiss_slam.config import load_config
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
-from rclpy.parameter import Parameter
 from sensor_msgs.msg import PointCloud2
-from std_msgs.msg import Header
+from std_msgs.msg import Empty, Header
 from tf2_ros import TransformBroadcaster
 
-from kiss_slam.occupancy_mapper import OccupancyGridMapper
 from kiss_slam.slam import KissSLAM
 
 
@@ -52,120 +52,147 @@ class KissSLAMNode(Node):
         """Create parameters, subscriptions, publishers, and set up SLAM"""
         super().__init__("kiss_slam_node")
 
-        # Create all the KISS-SLAM parameters in one go
-        self.declare_parameters("", slam_params_from_config(None))
-
         # Params specific to ROS wrapper
-        points_topic_desc = ParameterDescriptor(
-            description="What topic to listen on for PointCloud2 messages."
-        )
-        self.declare_parameter("points_topic", "/points", points_topic_desc)
+        self.declare_parameter("points_topic", "/points")
+        self.declare_parameter("map_frame", "map")
+        self.declare_parameter("position_covariance", 0.1)
+        self.declare_parameter("orientation_covariance", 0.1)
+
         self.points_topic = self.get_parameter("points_topic").value
-
-        map_frame_desc = ParameterDescriptor(
-            description="Frame ID to use as parent for SLAM estimated transform. Dynamic."
-        )
-        self.declare_parameter("map_frame", "map", map_frame_desc)
         self.map_frame = self.get_parameter("map_frame").value
-
-        position_covariance_desc = ParameterDescriptor(
-            description="Covariance value for position terms of odometry. Dynamic."
-        )
-        self.declare_parameter("position_covariance", 0.1, position_covariance_desc)
         self.position_covariance = self.get_parameter("position_covariance").value
-
-        orientation_covariance_desc = ParameterDescriptor(
-            description="Covariance value for orientation terms of odometry. Dynamic."
-        )
-        self.declare_parameter("orientation_covariance", 0.1, orientation_covariance_desc)
         self.orientation_covariance = self.get_parameter("orientation_covariance").value
 
-        # Main subscription to drive the SLAM process
-        self.subscription = self.create_subscription(
-            PointCloud2, self.points_topic, self.cloud_callback, 10
-        )
+        # Subscribers
+        self.cloud_sub = self.create_subscription(PointCloud2, self.points_topic, self.cloud_cb, 10)
+
+        self.done_sub = self.create_subscription(Empty, "done", self.done_cb, 10)
 
         # Publishers
         self.odom_publisher = self.create_publisher(Odometry, "odom", 10)
-        self.map_publisher = self.create_publisher(OccupancyGrid, "map", 10)
+        self.map_publisher = self.create_publisher(PointCloud2, "map", 10)
+        self.pose_publisher = self.create_publisher(PoseStamped, "estimated_pose", 10)
         self.tf_broadcaster = TransformBroadcaster(self)
 
-        # Pull slam config params and initialize
-        self.slam_config = slam_config_from_params(self)
-        self.slam = KissSLAM(self.slam_config)
-        self.mapper = OccupancyGridMapper(self.slam_config.occupancy_mapper)
+        self.kiss_slam_config = load_config(None)
+        self.slam = KissSLAM(self.kiss_slam_config)
+        self.freqs = []
+        self.timestamps = []
 
-        # Initial variables for mapping
-        self.ref_ground_alignment = None
-        self.min_voxel_idx = (0, 0)
+    def publish_map(self, points: np.ndarray):
+        header = Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = "/map"
+        map_msg = point_cloud2.create_cloud_xyz32(header, points[:, :3])
 
-        self.add_on_set_parameters_callback(self.parameter_callback)
+        self.map_publisher.publish(map_msg)
 
-    def parameter_callback(self, params: list):
-        """Callback to handle dynamic parameters for runtime reconfiguration
+    def publish_pose(self, pose: PoseStamped):
+        self.pose_publisher.publish(pose)
 
-        :param params: List of params changed that triggered this callback
-        :type params: list
-        :return: Success regardless of whether a param was updated
-        :rtype: SetParametersResult
-        """
-        for param in params:
-            if param.name == "map_frame" and param.type_ == Parameter.Type.STRING:
-                self.map_frame = param.value
-            if param.name == "position_covariance" and param.type_ == Parameter.Type.DOUBLE:
-                self.position_covariance = param.value
-            if param.name == "orientation_covariance" and param.type_ == Parameter.Type.DOUBLE:
-                self.orientation_covariance = param.value
-        return SetParametersResult(successful=True)
+    def cloud_cb(self, in_msg: PointCloud2):
+        start = self.get_clock().now()
+        # Store timestamp from message
+        timestamp = in_msg.header.stamp.sec + in_msg.header.stamp.nanosec * 1e-9
+        self.timestamps.append(timestamp)
 
-    def cloud_callback(self, in_msg: PointCloud2):
-        """When receiving a new point cloud, run KISS-SLAM and produce a tf and odometry
-        estimate in the same frame as the point source
-
-        :param in_msg: New points to injest
-        :type in_msg: PointCloud2
-        """
         pcd = pc2_to_numpy(in_msg)
         self.slam.process_scan(pcd, np.empty((0,)))
 
         pose = matrix_to_pose(self.slam.poses[-1])
+        pose_stamped = PoseStamped()
+        pose_stamped.header.frame_id = "/map"
+        pose_stamped.header.stamp = self.get_clock().now().to_msg()
+        pose_stamped.pose = pose
+        self.publish_pose(pose_stamped)
+
         header = Header()
         header.stamp = in_msg.header.stamp
         header.frame_id = self.map_frame
 
-        t = build_transform(header, pose, in_msg.header.frame_id)
-        self.tf_broadcaster.sendTransform(t)
+        # t = build_transform(header, pose, in_msg.header.frame_id)
+        # self.tf_broadcaster.sendTransform(t)
 
-        odom = build_odometry(header, pose, self.position_covariance, self.orientation_covariance)
-        self.odom_publisher.publish(odom)
+        # odom = build_odometry(header, pose, self.position_covariance, self.orientation_covariance)
+        # self.odom_publisher.publish(odom)
 
-        self.update_mapper(pcd)
-        map_msg = build_map(
-            header,
-            self.occupancy_2d,
-            self.min_voxel_idx,
-            self.slam_config.occupancy_mapper.resolution,
+        self.publish_map(self.slam.voxel_grid.point_cloud())
+
+        duration = self.get_clock().now() - start
+        elapsed_s = duration.nanoseconds * 1e-9
+        if elapsed_s > 0:
+            freq = 1.0 / elapsed_s
+            self.freqs.append(freq)
+
+    def done_cb(self, _: Empty):
+        self.slam.generate_new_node()
+        self.slam.local_map_graph.erase_last_local_map()
+        self.poses, self.pose_graph = self.slam.fine_grained_optimization()
+        self.poses = np.array(self.poses)
+
+        # Export poses to TUM format (matching pipeline implementation)
+        path = "/home/nicolas-lauzon"
+        tum_file = os.path.join(path, "trajectory_red.tum")
+
+        with open(tum_file, "w") as f:
+            for i, pose_matrix in enumerate(self.poses):
+                # Extract translation
+                tx, ty, tz = pose_matrix[:3, -1].flatten()
+
+                # Extract rotation and convert to quaternion (w, x, y, z) order
+                qw, qx, qy, qz = Quaternion(matrix=pose_matrix, atol=0.01).elements
+
+                # Use actual timestamp from message
+                timestamp = self.timestamps[i]
+
+                # Write in TUM format: timestamp tx ty tz qx qy qz qw
+                f.write(
+                    f"{timestamp:.4f} {tx:.4f} {ty:.4f} {tz:.4f} {qx:.4f} {qy:.4f} {qz:.4f} {qw:.4f}\n"
+                )
+
+        # Save map (point cloud) to CSV
+        # Create global map by transforming all local maps to global frame
+        all_points = []
+        keyposes = self.slam.get_keyposes()
+
+        for i, local_map in enumerate(self.slam.local_map_graph.local_maps()):
+            if i >= len(keyposes):
+                break
+
+            # Get the local map point cloud
+            local_pcd = local_map.pcd.point.positions.cpu().numpy()
+
+            # Transform to global frame using the keypose
+            R = keyposes[i][:3, :3]
+            t = keyposes[i][:3, -1]
+            global_points = local_pcd @ R.T + t
+
+            all_points.append(global_points)
+
+        # Combine all points
+        if all_points:
+            global_map = np.vstack(all_points)
+        else:
+            global_map = np.array([]).reshape(0, 3)
+
+        map_file = os.path.join(path, "map_red.csv")
+        np.savetxt(
+            map_file, global_map[:, :3], fmt="%.4f", delimiter=",", header="x,y,z", comments=""
         )
-        self.map_publisher.publish(map_msg)
 
-    def update_mapper(self, pcd: np.ndarray):
-        """Use a point cloud to update the node's OccupancyGridMapper and store the new 2d grid.
-        Also update the minimum active voxel xy idx so the map origin can be placed.
-
-        :param pcd: Latest point cloud received
-        :type pcd: np.ndarray
-        """
-        if self.ref_ground_alignment is None:
-            self.ref_ground_alignment = map_closures.align_map_to_local_ground(
-                self.slam.voxel_grid.open3d_pcd_with_normals().point.positions.cpu().numpy(),
-                self.slam_config.odometry.mapping.voxel_size,
+        log_file = os.path.join(path, "result_metrics_red.log")
+        with open(log_file, "w") as f:
+            f.write("────────────────── Results ────────────────────\n")
+            f.write("+--------------------------+-------+----------+\n")
+            f.write("|                   Metric | Value | Units    |\n")
+            f.write("+==========================+=======+==========+\n")
+            f.write(f"|          Average freq |  {np.mean(self.freqs):.0f}   | Hz       |\n")
+            f.write(
+                f"| Number of closures found |   {len(self.slam.get_closures())}   | closures |\n"
             )
+            f.write("+--------------------------+-------+----------+\n")
 
-        self.mapper.integrate_frame(pcd, self.ref_ground_alignment @ self.slam.poses[-1])
-        self.mapper.compute_3d_occupancy_information()
-        self.mapper.compute_2d_occupancy_information()
-        self.occupancy_2d = self.mapper.occupancy_grid
-        self.min_voxel_idx = (self.mapper.lower_bound[0], self.mapper.lower_bound[1])
+        self.get_logger().info(f"Exported {len(self.poses)} poses to {tum_file}")
 
 
 def main(args=None):
